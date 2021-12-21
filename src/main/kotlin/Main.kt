afba1b42ba41fb1cdca2fd0d408d04de26ccf68a
Lucas Felici <lucas.a.felici@gmail.com>
2021-12-21 11:00:36 -0300

    primer commit

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,4 +1,11 @@
 fun main(args: Array<String>) {
 
     //Prueba de push
+    val vehiculo1: Vehicle = Vehicle("HJU667",VehicleType.CAR,"Card01")
+    val vehiculo2 = Vehicle("JJU456", VehicleType.MOTORCYCLE)
+    val vehiculo3 = Vehicle("FGR234", VehicleType.BUS, "Card02")
+    val vehiculo4 = Vehicle("JUJ672", VehicleType.MINIBUS)
+    println(vehiculo1.plate)
+    println("Tipo: ${vehiculo1.type} Tarifa primeras 2 hs: $${vehiculo1.type.tarifa}")
+    println("Ingreso: ${vehiculo1.checkInTime.time}")
 }
--- src/main/kotlin/Vehicle.kt
+++ src/main/kotlin/Vehicle.kt
@@ -1,14 +1,34 @@
-data class Vehicle (
-    val plate : String
-        ) {
+import java.util.*
 
-        override fun equals(other: Any?): Boolean {
-                if(other is Vehicle){
-                        return this.plate == other.plate
-                }
-                return super.equals(other)
+data class Vehicle(
+    val plate: String,
+    val type: VehicleType,
+    val discountCard: String? = null,
+    val checkInTime: Calendar = Calendar.getInstance()
+) {
+
+    companion object{
+        const val MINUTES_IN_MLS = 1000
+    }
+
+    val parkedTime: Long get() = (Calendar.getInstance().timeInMillis - checkInTime.timeInMillis) / MINUTES_IN_MLS
+    override fun equals(other: Any?): Boolean {
+        if (other is Vehicle) {
+            return this.plate == other.plate
         }
+        return super.equals(other)
+    }
 
-        override fun hashCode(): Int = this.plate.hashCode()
+    override fun hashCode(): Int = this.plate.hashCode()
 
 }
+
+//el type puede estar prefijado
+
+//cupo maximo de 20
+//auto: $20, moto: $15, mini bus: $25, bus: $30
+//$5 por cada 15min extra!
+//Tarjeta de descuento del 15%
+//puedo listar siempre los autos que hay
+//fecha de ingreso
+//calcular tiempo total de estacionamiento
